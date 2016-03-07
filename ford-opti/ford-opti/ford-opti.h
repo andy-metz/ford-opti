22a7ce3e330916781ad46a3509f2dea1e54b1a3b
andre <andre.erba@neuf.fr>
2016-03-07 10:36:12 +0100

    Première version tournant

--- ford-opti/ford-opti/ford-opti.h
+++ ford-opti/ford-opti/ford-opti.h
@@ -1,6 +1,11 @@
 // opti
 #include <string>
 #include <iostream>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <math.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -36,6 +41,9 @@ public:
 
 	void setWeight(double w){
 		weight = w;
+		}
+	double getWeight(){
+		return weight;
 	}
 
 };
@@ -87,23 +95,37 @@ class Pred{
 };
 
 // List of successors for each vertex of a graph
-class 
+class Successors{
+public:
+	string value;
+	double w;
+	Successors *next;
+};
+// 
 // 
 
 
-
-
+//
 // Graph Class
+//
 class Graph{
 
 private:
-	Edge * Start_edge;
+	Vertex * start_vertex;
 	Vertices_list *vertices_list = NULL;
 	Edges_list *edges_list = NULL;
+	// Concern Shortest path
+	map<string, double> lambda;
+	unordered_set<string> *marked_vertex;
+	unordered_set<string> *alternate_marked_vertex;
+	map<string, Successors *> successors_list;
+	map<string, string> predecessor;
+
+
 
 public:
 	Graph(){
-		Start_edge = NULL;
+		start_vertex = NULL;
 	}
 
 	// Add a vertex
@@ -124,12 +146,31 @@ public:
 		return v;
 	}
 
+	string getPredecessor(string x){
+		return predecessor[x];
+	}
+
+	void setStartVertex(string s){
+		start_vertex = add_vertex(s);
+	}
+
 	Vertices_list * getVertices_list(){
 		return(vertices_list);
 	}
 
 
+	int getNumberOfVertex(){
+		int n = 0;
+		Vertex * v;
 
+		Vertices_list *vl;
+		vl = vertices_list;
+		while (vl){
+			n++;
+			vl = vl->next;
+		}
+		return n;
+	}
 
 
 	// Add an edge
@@ -186,12 +227,108 @@ public:
 	}
 
 
-	// algo Ford-Bellman optimisé
+	// relax
+
+
+	// algo Ford-Bellman optimized
 	void shortest_path(){
-		// initialisation
+		Vertices_list * vl = vertices_list;
+		Edges_list * el;
+		Edge *e;
+		Successors * s;
+		map<string, Successors *>::iterator it;
+		// initialisation successor_list
+		el = edges_list;
+		while (edges_list){
+			e = edges_list->edge;
+			s = new Successors();
+			// s->next = NULL;
+			s->value = e->getVertexDestination()->getName();
+			s->w = e->getWeight();
+			it = successors_list.find(e->getVertexSource()->getName());
+			if (it == successors_list.end()){  // doesn't exists
+				s->next = NULL;
+
+
+			}
+			else{
+				s->next = it->second;
+			}
+			successors_list[e->getVertexSource()->getName()] = s;
+
+			edges_list = edges_list->next;
+		}
 
 
 
+		int k = 0;
+		int n = this->getNumberOfVertex();
+
+		marked_vertex = new unordered_set<string>;
+		alternate_marked_vertex = new unordered_set<string>;
+
+		while (vl)
+		{
+			lambda[vl->vertex->getName()] = DBL_MAX; // Double max afffectation to all vertex 
+			predecessor[vl->vertex->getName()] = "";
+			vl = vl->next;
+		}
+
+		//
+		lambda[this->start_vertex->getName()] = 0;
+
+		// init marked_vertex
+		marked_vertex->insert(start_vertex->getName());
+
+
+		while ((k < n) && !marked_vertex->empty()){
+			k++;
+			alternate_marked_vertex->clear();
+
+
+			for (auto itr = marked_vertex->begin(); itr != marked_vertex->end(); ++itr){
+
+				it = successors_list.find(*itr);
+				if (it != successors_list.end()){
+					Successors *next_successor;
+					next_successor = it->second;
+					while (next_successor)
+					{
+
+						if (lambda[*itr] + next_successor->w < lambda[next_successor->value]){ // Better path
+							predecessor[next_successor->value] = *itr;
+							lambda[next_successor->value] = lambda[*itr] + next_successor->w;
+							alternate_marked_vertex->insert(next_successor->value);
+						}
+						cout << endl << next_successor->value;
+
+
+
+						next_successor = next_successor->next;
+					}
+				}
+
+				//set<string>::const_iterator mkit(marked_vertex.begin()), mkend(marked_vertex.end());
+				//for (; mkit != mkend; ++mkit){
+				/*
+				for_each(marked_vertex->begin(), marked_vertex->end(), [](string v){
+
+				it = successors_list.find(e->getVertexSource()->getName());
+				if (it == successors_list.end()){  // doesn't exists
+				s->next = NULL;
+				cout << v;
+
+
+				}
+
+				);*/
+			}
+
+		// 
+		marked_vertex->clear();
+		for (auto itr = alternate_marked_vertex->begin(); itr != alternate_marked_vertex->end(); ++itr)
+		marked_vertex->insert(*itr);
+		}
 
 	}
 };
--- ford-opti/ford-opti/main.cpp
+++ ford-opti/ford-opti/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "ford-opti.h"
+#include <map>
+#include <string>
 
 using namespace std;
 int main()
@@ -10,15 +12,37 @@ int main()
 
 
 
-	g->add_edge("A", "B", 10.0);
+	g->add_edge("A", "B", 4.0);
 	g->add_edge("A", "C", 2.0);
-	g->add_edge("C", "E", 5.0);
-	g->add_edge("B", "E", 7.0);
+	g->add_edge("C", "D", 2.0);
+	g->add_edge("B", "D", 1.0);
 
-
+	/*
 	cout << g->getVertices_list()->getVertexList();
 
-	cout << "Fin" << endl;
+	map<string, Vertex *> dict;
+	dict["A"] = g->getVertex("A");
+
+
+	cout << "Test hash:" << endl;
+	Vertex * v = dict["D"];
+
+	if (v)
+		cout << v->getName() << endl;
+	//cout << v.getName()) << "\n";
+	*/
+
+	cout << "Nombre sommets:" << g->getNumberOfVertex() << endl;
+	//std::map<string, Vertex> x = { { "A", g->getVertex("A") }, { "B", g->getVertex("B") }, { "C", g->getVertex("C") } };
+	//map<string, Vertex> x = { { "A", g->getVertex("A") }, { "B", g->getVertex("B") }, { "C", g->getVertex("C") } };
+
+	g->setStartVertex("A");
+
+	g->shortest_path();
+
+
+	cout <<  "predecessor:" << g->getPredecessor("D");
+	cout << endl << "Fin" << endl;
 	system("pause");
 
 }
